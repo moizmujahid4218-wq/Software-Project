f79b0b62bf7ce1b7e16ed2739bf4e6c09d1d9650
Suixide Squad <moizmujahid4218@gmail.com>
2026-06-19 20:45:31 +0500

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,5 @@ class Config:
     # Secret key for session management
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'super-secret-bakery-key'
     
-    # Database Configuration
-    DB_HOST = 'localhost'
-    DB_USER = 'root'
-    DB_PASSWORD = '1234' # Leave blank if you don't have a password on local mysql
-    DB_NAME = 'bakery_db'
+    # We no longer need MySQL credentials! We just tell SQLite where to put the file.
+    DATABASE = 'bakery.db'
